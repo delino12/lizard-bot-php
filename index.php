4e711b56496031836f89a505776ec72cde3eeb85
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-07-12 12:35:09 +0100

    update index files

--- index.php
+++ index.php
@@ -1,23 +1,28 @@
 <?php
-include ("__engine");
+# include the bot __engine.php script
+require('__engine.php');
 
+# verify the page set tokem and page access token
 $VERIFY_TOKEN = 'delino12';
-$PAGE_ACCESS_TOKEN = 'EAAPqY1IUo8cBAFE9jpJftQh8mvp8l0mJfdqeZBjFFIqvWYmPYGXeZBNFjWVJLTeqF9D7BiVeG4NKrxukELbghVZCQGWDYM6i4daRhY3pNu8YAenXKgSBzyQbwjsazJ5aKg1C6rAkmUkNsdDqWs3abK8n2vgsCMtrzDkhm9gDwZDZD';
+$PAGE_ACCESS_TOKEN = 'EAAPqY1IUo8cBAE6Sev039mH6uIZAdTnAar6dwr7WphlepgPFpaxtVWQdRA6X7n9sg8izl1J4wZBELax6U3XrFcUWDmfjMP3RMs8DY1EE7lziyErZCtawOkzijA3mVoUcsEUH9VhG8DQlzBAUrqac1JVpsBdjgSaZBO9Fm4nD8AZDZD';
 
+# facebook init challange matchs
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
 if ($verify_token === $VERIFY_TOKEN) {
   	//If the Verify token matches, return the challenge.
   	echo $challenge;
 }else {
-
+	
+	# scan bot input for incomming request
 	$input = json_decode(file_get_contents('php://input'), true);
 	// Get the Senders Page Scoped ID
 	$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 	// Get the message text sent
 	$message = $input['entry'][0]['messaging'][0]['message']['text'];
-	
-	$lizards_talks = new LizardTalk($message, $sender, $PAGE_ACCESS_TOKEN);
-	$lizards_talks->talkback();
+
+	# custom define bot class for handling response
+	$lizads_talks = new LizardTalk($message, $sender, $PAGE_ACCESS_TOKEN);
+	$lizads_talks->talkback();
 }
 ?>
