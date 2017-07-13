0a4408acb201c9065238273524f0f898a73b8e27
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-07-13 15:22:29 +0100

    removing __app.php from app dir

--- __engine.php
+++ __engine.php
@@ -1,29 +1,6 @@
 <?php
 # include functions 
 require('__core.php');
-//require('__app.php');
-
-# octa structure of A.I
-# welcoming words
-$words = array(
-	'hi'		=> 	' Welcome to Ama Technology, we offer Assistance to PHP developer Bot using facebook messenger bot kit',
-	'hey'		=> 	' Welcome to Ama Technology, we offer Assistance to PHP developer Bot using facebook messenger bot kit',
-	'hello'		=> 	' Welcome to Ama Technology, we offer Assistance to PHP developer Bot using facebook messenger bot kit',
-	'time' 		=> 	' The time is "'.date(' h:i:s').'" ',
-	'joke' 		=> 	' Do you know that all redneck lizards in Nigeria always nod their head even when Nigeria economy is very bad!',
-	'day' 		=> 	' Today: "'.date('D').'" ',
-	'tips'		=> 	'  going to teach you some study tricks, how to dessolve complex learning materials',
-	'ok'		=> 	' Thanks for using lizards service you can always come back for more.....',
-	'proceed'	=>	' step 1 goes here........',
-	'start'		=>	' learning tips to learn more quicly and more deeply 
-					  \n, pay attention to how you pay attention, 
-					  \n Think about how you think. 
-					  \nLearn how you learn.',
-	'one'		=>	' Here is step 1',
-	'date'		=>	' Today Date is "'.date(' M D Y').'" ',
-	'image'		=> 	' https://i.warosu.org/data/tg/img/0261/33/1374363369802.jpg '
-);
-
 
 /**
 * Lizards Bot
@@ -45,28 +22,18 @@ class LizardTalk
 	public function talkback()
 	{
 		if(!empty($this->message)){
-			// check if image response need image files 
-			if(in_array($this->message, $words)){
-				$response = $words[$this->message];
+			
+			if($this->message == 'hi' || $this->message == 'hello' || $this->message == 'hey'){
+				$response = 'Welcome to Ama Technology, we offer Assistance to PHP developer, Developing Bot using facebook messenger bot kit ';
 				send_text_message($this->sender, $response, $this->page_access_token);
-
 			}
 
-			/*
-			if(in_array($this->message, $words))
-			{
-				$response = $words[$this->message];
-				send_image_message($this->sender, $response, $this->page_access_token);
-			}
-			*/
-
-			/*
 			if($this->message == 'image'){
 				$image_url = 'https://i.warosu.org/data/tg/img/0261/33/1374363369802.jpg';
 				send_image_message($this->sender, $image_url, $this->page_access_token);
 			}
 
-			if($this->message == 'hi'){
+			if($this->message == 'info'){
 				$web_url = 'http://www.amatechteam.com';
 				$text = 'Welcome to Ama Technology, we provide Bot Assistance for your business';
 				send_button_message($this->sender, $web_url, $text, $this->page_access_token);
@@ -76,10 +43,8 @@ class LizardTalk
 				$web_url = 'http://www.amatechteam.com';
 				$image_url = 'https://i.warosu.org/data/tg/img/0261/33/1374363369802.jpg';
 				$subtitle_text = 'Lizard Bot offers PHP Flexibility';
-				$body_text = 'You can always ask for more lizard spy bot are everywhere. there is no wall above me. ';
-				send_generic_message($sender, $subtitle_text, $image_url, $web_url, $this->page_access_token);
+				send_generic_message($this->sender, $subtitle_text, $image_url, $web_url, $this->page_access_token);
 			}
-			*/
 		}
 	}
 }
