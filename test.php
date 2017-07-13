4c54f009d4396e917bc0e5934181b1ed31eef930
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-07-13 14:19:43 +0100

    delete test-1 script allowing test.php to function as a work sheet when programmming your bot

--- test.php
+++ test.php
@@ -3,26 +3,5 @@
 require('__engine.php');
 
 # verify the page set tokem and page access token
-$VERIFY_TOKEN = 'delino12';
-$PAGE_ACCESS_TOKEN = 'EAAPqY1IUo8cBAE6Sev039mH6uIZAdTnAar6dwr7WphlepgPFpaxtVWQdRA6X7n9sg8izl1J4wZBELax6U3XrFcUWDmfjMP3RMs8DY1EE7lziyErZCtawOkzijA3mVoUcsEUH9VhG8DQlzBAUrqac1JVpsBdjgSaZBO9Fm4nD8AZDZD';
-
-# facebook init challange matchs
-$challenge = $_REQUEST['hub_challenge'];
-$verify_token = $_REQUEST['hub_verify_token'];
-if ($verify_token === $VERIFY_TOKEN) {
-  	//If the Verify token matches, return the challenge.
-  	echo $challenge;
-}else {
-	
-	# scan bot input for incomming request
-	$input = json_decode(file_get_contents('php://input'), true);
-	// Get the Senders Page Scoped ID
-	$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-	// Get the message text sent
-	$message = $input['entry'][0]['messaging'][0]['message']['text'];
-
-	# custom define bot class for handling response
-	$lizads_talks = new LizardTalk($message, $sender, $PAGE_ACCESS_TOKEN);
-	$lizads_talks->talkback();
-}
+# this test page exit for testing webhook when programming your bots
 ?>
