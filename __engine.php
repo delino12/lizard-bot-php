e990d616dd707c36aec2251452013c702c9ed627
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-07-15 16:48:03 +0100

    added jokes

--- __engine.php
+++ __engine.php
@@ -23,12 +23,29 @@ class LizardTalk
 	{
 		if(!empty($this->message)){
 			
-			$word_lists = array('hi','hey','hello','generic','image','info','date','location','details','help');
+			$word_lists = array('hi','hey','hello','generic','image','info','date','location','details','help','jokes');
+			$jokes = array(
+				'3 men eating breakfast with their wives, 1st man sed, pass the honey honey 2nd man sed, pass the sugar sugar & 3rd man sed,pass the milk,you big fat cow!',
+				'TEACHER: Mary, why are you doing your math multiplication on the floor? MARY: You told me to do it without using tables.',
+				'Your 100% beautiful Your 100% lovely Your 100% sweet Your 100% nice & your 100% stupid to believe these words!',
+				'What kind of streets do zombies like the best?… Dead ends',
+				'My girlfriend always complains that I do not take her anywhere expensive.. So I took her to the Petrol Station.'
+			);
+
+			$jokes_total = count($jokes);
+			$jokes_total = $jokes_total - 1;
+			$shuffle_jokes = rand(0, $jokes_total);
 
 			if(in_array($this->message, $word_lists)){
 				# start response
 				if($this->message == 'hi' || $this->message == 'hello' || $this->message == 'hey'){
 					$response = 'Welcome to Ama Technology, we offer Assistance to PHP developer, Developing Bot using facebook messenger bot kit ';
+					send_text_message($this->sender, $response, $this->page_access_token);
+				}
+
+				# start response
+				if($this->message == 'jokes'){
+					$response = $jokes[$shuffle_jokes];
 					send_text_message($this->sender, $response, $this->page_access_token);
 				}
 
--- test.php
+++ test.php
@@ -13,4 +13,26 @@ if(in_array($words, $word_lists)){
 }
 echo '<br />';
 echo date(" F D h:i:s a");
+
+
+echo '<br />';
+
+$jokes = array(
+	'3 men eating breakfast with their wives, 1st man sed, pass the honey honey 2nd man sed, pass the sugar sugar & 3rd man sed,pass the milk,you big fat cow!',
+	'TEACHER: Mary, why are you doing your math multiplication on the floor? MARY: You told me to do it without using tables.',
+	'Your 100% beautiful Your 100% lovely Your 100% sweet Your 100% nice & your 100% stupid to believe these words!',
+	'What kind of streets do zombies like the best?… Dead ends',
+	'My girlfriend always complains that I do not take her anywhere expensive.. So I took her to the Petrol Station.'
+);
+
+
+$jokes_total = count($jokes);
+$jokes_total = $jokes_total - 1;
+$shuffle_jokes = rand(0, $jokes_total);
+
+echo $jokes[$shuffle_jokes];
+
+echo '<br /> shuffle no: '.$shuffle_jokes;
+
+
 ?>
